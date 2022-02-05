20a68aea15036b2247d69a480308a19a051a25ee
Rocklee9979 <48349639+Rocklee9979@users.noreply.github.com>
2022-02-05 15:49:25 +1100

    added orbit controls

--- test/vite-project/js/main.js
+++ test/vite-project/js/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import * as dat from 'dat.gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const gui = new dat.GUI();
 const world = {
@@ -48,18 +49,14 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
+
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(renderer.domElement);
-
-// const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const mesh = new THREE.Mesh(boxGeometry, material);
-// scene.add(mesh);
-
+const control = new OrbitControls(camera, renderer.domElement);
 
 const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
@@ -87,6 +84,10 @@ for (let i = 0; i < array.length; i += 3){
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
 scene.add(light);
+
+const backlight = new THREE.DirectionalLight(0xffffff, 1);
+backlight.position.set(0, 0, -1);
+scene.add(backlight);
 
 
 function animate() {
