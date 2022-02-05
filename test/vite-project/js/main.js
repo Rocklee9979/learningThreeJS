e7dd18f36301a9bb37b78d08f01d0ccf4b471631
Rocklee9979 <48349639+Rocklee9979@users.noreply.github.com>
2022-02-05 15:40:27 +1100

    dat.gui installed and used a little bit
    dat.gui allows us to modify the slider as per our requirements

--- test/vite-project/js/main.js
+++ test/vite-project/js/main.js
@@ -1,41 +1,99 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import WebGL from './WebGL';
+import * as dat from 'dat.gui';
+
+const gui = new dat.GUI();
+const world = {
+    plane: {
+        width: 10,
+        height: 10,
+        widthSegments: 10, 
+        heightSegments: 10,
+    }
+};
+gui.add(world.plane, 'height', 1, 50).onChange(() => {
+    generetePlane();
+});
+gui.add(world.plane, 'width', 1, 20).onChange(() => {
+    generetePlane();
+});
+gui.add(world.plane, 'widthSegments', 1, 50).onChange(() => {
+    generetePlane();
+});
+gui.add(world.plane, 'heightSegments', 1, 50).onChange(() => {
+    generetePlane();
+});
+
+function generetePlane() {
+    planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.PlaneGeometry(
+        world.plane.width,
+        world.plane.height,
+        world.plane.widthSegments,
+        world.plane.heightSegments
+    );
+    
+    let array = [];
+    array = planeMesh.geometry.attributes.position.array;
+    
+    for (let i = 0; i < array.length; i += 3){
+        const x = array[i];
+        const y = array[i + 1];
+        const z = array[i + 2];
+    
+        array[i + 2] = z + Math.random();
+    }
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material); 
-scene.add(cube);
+// const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const mesh = new THREE.Mesh(boxGeometry, material);
+// scene.add(mesh);
 
-camera.position.z = 5; 
+
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+    color: 0xff0000,
+    side: THREE.DoubleSide,
+    flatShading: THREE.FlatShading
+});
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+let array = [];
+array = planeMesh.geometry.attributes.position.array;
+
+for (let i = 0; i < array.length; i += 3){
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random();
+}
+
+
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
-}
+    // planeMesh.rotation.x += 0.01;
+   
+};
 
-
-
-
-if ( WebGL.isWebGLAvailable() ) {
-
-	// Initiate function or other initializations here
-	animate();
-
-} else {
-
-	const warning = WebGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
-
-}
+animate();
